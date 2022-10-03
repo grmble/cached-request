b33c98875b6d5d1aced973e1ddcefd6e9b5b6553
Juergen Gmeiner <spamless.juergen@gmail.com>
2022-10-03 12:42:48 +0200

    lein new grmble/cached-request

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cached-request
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/grmble/cached_request_test.clj
@@ -0,0 +1,7 @@
+(ns grmble.cached-request-test
+  (:require [clojure.test :refer :all]
+            [grmble.cached-request :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
